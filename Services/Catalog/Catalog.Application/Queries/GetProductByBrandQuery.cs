964f5cff5b1ae7f7fede0c91e79c0fc5b4eb9627
Emin Akyüz <emn.ak100@gmail.com>
2024-12-24 02:02:10 +0300

    UpdateProductHandler and GetProductByBrandHandler

---
+++ Services/Catalog/Catalog.Application/Queries/GetProductByBrandQuery.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries;
+
+public class GetProductByBrandQuery:IRequest<IList<ProductResponse>>
+{
+	public string Brandname { get; set; }
+	public GetProductByBrandQuery(string brandname)
+	{
+		Brandname = brandname;
+	}
+}
