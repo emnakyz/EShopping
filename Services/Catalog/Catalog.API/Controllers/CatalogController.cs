61cebe1e24c717920d862c733fcfa92df0708a18
Emin Akyüz <emn.ak100@gmail.com>
2025-01-13 01:21:35 +0300

    GetProductById  Controller

--- Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Catalog.API.Controllers
 {
 
 	public class CatalogController : ApiController
 	{
+		private readonly IMediator _mediator;
+		public CatalogController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+		[HttpGet]
+		[Route("[action]/{id}", Name = "GetProductById")]
+		[ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<ProductResponse>> GetProductById(string id)
+		{
+			var query = new GetProductByIdQuery(id);
+			var result = await _mediator.Send(query);
+			return Ok(result);
+		}
 	}
 }
